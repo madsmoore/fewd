83e4467acf83983b15760763360690c794243003
madsmoore <madisonmoore284@gmail.com>
2016-01-03 14:02:59 -0800

    sdfds

--- homework/movie-search/movie-search.js
+++ homework/movie-search/movie-search.js
@@ -1,33 +1,54 @@
-// This is what the data returning from IMDB will look like:
-var sampleResult = {
-  "Search": [
-    {
-      "Title": "Cool Runnings",
-      "Type": "movie",
-      "Year": "1993",
-      "imdbID": "tt0106611"
-    }
-  ]
-}
+// In jQuery terms:
 
-// Attach an event listener to the form submit (using jQuery)
-$("#movie-search-form").submit(formSubmitted);
+var form = document.querySelector("#movie-search-form");     // $("#movie-search-form").on("submit", formSubmitted)
+form.addEventListener("submit", formSubmitted);
 
-// Handle the form submission: go to OMDB and get results
 function formSubmitted(event) {
-  event.preventDefault();
-  var url = "http://omdbapi.com/?s=" + $("#query").val();
-  $.get(url, resultsReceived);
+event.preventDefault();
+var query = document.querySelector("#query").value;        // $("#query").val()
+var url = "http://omdbapi.com/?s=" + query;
+$.get(url, resultsReceived);
 }
 
 function resultsReceived(results) {
-  // Try putting a debugger here and inspecting the results argument
-  // The array of movies lives inside results["Search"]
-  // See the sampleResult above for an example
+results["Search"].forEach(displayMovie);
+}
 
-  // Access the array of movies:
-  results["Search"]
+function displayMovie(movie) {
+// Create new elements (x4)                                // In jQuery terms:
+var item = document.createElement("li");                   // $("<li>")
+var link = document.createElement("a");                    // $("<a>")
+var title = document.createElement("div");                 // $("<div>")
+var release = document.createElement("div");
 
-  // Access the first movie title
-  results["Search"][0]["Title"]
+// Set element text content (x2)
+link.textContent = movie["Title"];                         // item.text(movie["Title"])
+release.textContent = movie["Year"];
+
+// Set element attributes (x2)
+var url = "http://www.imdb.com/title/" + movie["imdbID"];
+link.setAttribute("href", url);                            // link.attr("href", url)
+link.setAttribute("target", "_blank");
+
+// Add CSS classes to elements (x3)
+item.classList.add("movie");                               // item.addClass("movie")
+title.classList.add("movie-title");
+release.classList.add("movie-release-date");
+
+// Append elements (x4)
+title.appendChild(link);                                   // title.append(link)
+item.appendChild(title);
+item.appendChild(release);
+document.querySelector("#movies").appendChild(item);       // item.appendTo("#movies")
+
+// Full jQuery version of this function:
+//
+// var item = $("<li>").addClass("movie").appendTo("#movies");
+// var title = $("<div>").addClass("movie-title").appendTo(item);
+//
+// var url = "http://www.imdb.com/title/" + movie["imdbID"];
+//
+// $("<a>").attr("href", url).attr("target", "_blank").text(movie["Title"]).appendTo(title);
+//
+// $("<div>").text(movie["Year"]).addClass("movie-release-date").appendTo(item);
 }
